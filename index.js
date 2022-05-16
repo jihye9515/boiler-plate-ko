6846d7d1e4ffac4c9c92a2c8f120c55170616f80
jihye_2 <ljh9515@gmail.com>
2022-05-16 16:05:26 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/dev')
+
+const {User} = require("./models/user")
+
+//bodyParser : 클라이언트에서 오는 정보를 서버에서 분석해서 가져올수 있게 해주는 것
+
+//application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올수 있게 하는것
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json 타입으로 된 데이터를 분석해서 가져올수 있게 하는것
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jihey:gkgk1352@boilerplate.ootow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true
 }).then(()=>console.log('mongoDB Connected..'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 안녕앙녕녕녕')
+    res.send('Hello World!  안녕안녕~')
+})
+
+app.post('/register', (req, res)=>{
+    //회원 가입 할때 필요한 정보들을 클라이언트에서 가져오면
+    //그것들을 데이터베이스에 넣어준다
+
+    const user = new User(req.body);
+
+    user.save((err,doc,userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+
+    })
 })
 
 
